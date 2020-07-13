166cea51e73677885f8464351fc9d7e35d7d46f2
Leo <ronald.mauritsson@gmail.com>
2020-07-14 01:24:24 +0300

    Modified App component and imported the calculate function

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,35 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import '../App.css';
+import Calculate from '../logic/calculate';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 
-function App() {
-  return (
-    <div className="App">
-      <Display />
-      <ButtonPanel />
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  handleClick = buttonName => {
+    const { total, next, operation } = this.state;
+    const result = Calculate({ total, next, operation }, buttonName);
+    this.setState(result);
+  }
+
+  render() {
+    const { total, next, operation } = this.state;
+    return (
+      <div className="App">
+        <Display result={total} operator={operation} next={next} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </div>
+    );
+  }
+}
 export default App;
